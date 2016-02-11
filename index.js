863a0a0b71df82fa6cf77c8cbcd5a3ed74e4ee77
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-02-11 13:46:53 +0800

    Improve error output again

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ function tapSpec (options) {
 
   if (!options.min) {
     tap.on('fail', function (t) {
-      out.push('  ' + s.err(symbols.cross) + ' ' + s.err(t.name) + '\n')
+      out.push('  ' + s.err(symbols.cross) + ' ' + s.test(t.name) +
+        ' ' + s.err(symbols.arrowRight + ' ' + t.error.operator) + '\n')
     })
 
     tap.on('pass', function (t) {
@@ -54,23 +55,24 @@ function tapSpec (options) {
       stream.failed = true
     }
 
-    if (!options.min) out.push('\n')
+    out.push('\n')
 
     if (results.fail.length > 0) {
-      if (!options.min) {
-        out.push('  ' + s.err(line()) + '\n')
-      }
+      out.push('  ' + chalk.bold('Failures:') + '\n')
 
       results.fail.forEach(function (t) {
         out.push('\n  ' + s.err(symbols.cross) + ' ' + s.err(t.name) + '\n')
-        out.push(formatErr(t.error))
+        try {
+          out.push(formatErr(t.error))
+        } catch (e) {
+          console.log(e)
+        }
       })
 
       out.push('\n')
 
       out.push('  ' +
-        s.err(symbols.cross) + ' ' +
-        s.err(results.fail.length + ' failed') + '\n    ' +
+        s.mute(results.fail.length + ' failed,') + ' ' +
         s.mute(results.pass.length + ' passed') +
         '\n')
     } else {
@@ -88,8 +90,13 @@ function formatErr (error) {
 
   var err = yaml.safeLoad(error.raw)
 
+  var in1 = '  '
+  var in2 = '    '
+  var in3 = '      '
+  var inE = in2 + s.err('- ') // Expected
+  var inA = in2 + s.ok('+ ') // Actual
   var out = ''
-  out += '    ' +
+  out += in2 +
     s.trace(relative(error.at.file) +
     ':' + error.at.line +
     ':' + error.at.character + ':')
@@ -97,17 +104,17 @@ function formatErr (error) {
   if ((~['equal', 'deepEqual', 'deepLooseEqual'].indexOf(err.operator)) ||
     (err.operator === 'throws' && err.expected)) {
     out += ' ' + s.trace(err.operator) + '\n'
-    out += '    ' + s.err('- ') + err.expected.replace(/\n/g, '\n      ') + '\n'
-    out += '    ' + s.ok('+ ') + err.actual.replace(/\n/g, '\n      ') + '\n'
+    out += inE + str(err.expected).replace(/\n/g, '\n' + inE) + '\n'
+    out += inA + str(err.actual).replace(/\n/g, '\n' + inA) + '\n'
   } else if (~['notEqual', 'notDeepEqual', 'notDeepLooseEqual'].indexOf(err.operator)) {
     out += ' ' + s.trace(err.operator) + '\n'
-    out += '    ' + s.ok('> ') + err.actual.replace(/\n/g, '\n      ') + '\n'
+    out += inA + str(err.actual).replace(/\n/g, '\n' + inA) + '\n'
   } else {
-    out += ' ' + s.trace(err.operator) + '\n'
+    out += in1 + s.trace(err.operator) + '\n'
   }
   if (error.stack) {
     // TODO: clean stack trace
-    out += '    ' + s.trace(error.stack.replace(/\n/g, '\n      '))
+    out += in2 + s.trace(error.stack.replace(/\n/g, '\n' + in3))
   }
 
   return out
@@ -117,11 +124,9 @@ function relative (path) {
   return path.replace(process.cwd() + require('path').sep, '')
 }
 
-function line () {
-  if (process.stdout.columns) {
-    return Array(process.stdout.columns - 3).join(symbols.line)
-  }
-  return Array(3).join(symbols.line)
+function str (object) {
+  if (typeof object === 'string') return object
+  return require('util').inspect(object, { depth: null })
 }
 
 module.exports = tapSpec
